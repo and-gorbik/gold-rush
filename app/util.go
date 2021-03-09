6f191d41e70dcf94823ebbdc170e5c1aefb36dc3
and-gorbik <and.gorbik@gmail.com>
2021-03-09 22:51:27 +0300

    add http client building

--- app/util.go
+++ app/util.go
@@ -1,11 +1,22 @@
 package app
 
 import (
+	"net/http"
+	"time"
+
 	"gold-rush/models"
 )
 
-func retry(attemps int) {
+func buildHTTPClient(timeout time.Duration) *http.Client {
+	t := http.DefaultTransport.(*http.Transport).Clone()
+	t.MaxIdleConns = maxIdleConns
+	t.MaxConnsPerHost = maxConnsPerHost
+	t.MaxIdleConnsPerHost = maxIdleConnsPerHost
 
+	return &http.Client{
+		Timeout:   timeout,
+		Transport: t,
+	}
 }
 
 func readError(err error) (string, bool) {
