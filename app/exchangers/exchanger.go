055dd6b1efb91092e3c6c18af6e205ac05dab094
and-gorbik <and.gorbik@gmail.com>
2021-03-22 21:31:34 +0300

    implement exchangers

--- app/exchangers/exchanger.go
+++ app/exchangers/exchanger.go
@@ -1,19 +1,54 @@
 package exchangers
 
 import (
+	"time"
+
 	"gold-rush/config"
+	"gold-rush/models"
 )
 
 type TreasuresExchanger struct {
 	provider provider
+	done     chan struct{}
 }
 
-func NewTreasuresExchanger(cfg config.Entity, provider provider, coins chan<- int, treasures <-chan []string) *TreasuresExchanger {
-	return &TreasuresExchanger{}
+func NewTreasuresExchanger(cfg config.Entity, provider provider, treasures <-chan []string, coins chan<- int) *TreasuresExchanger {
+	t := &TreasuresExchanger{
+		provider: provider,
+		done:     make(chan struct{}),
+	}
+
+	go t.run(treasures, coins)
+	return t
+}
+
+// на каждый сундук создается воркер, ответственный за обмен этого сундука на деньги
+func (t *TreasuresExchanger) run(treasuresChan <-chan []string, coins chan<- int) {
+	for treasures := range treasuresChan {
+		for _, treasure := range treasures {
+			go t.cash(treasure, coins)
+		}
+	}
+}
+
+func (t *TreasuresExchanger) cash(treasure string, coins chan<- int) {
+	retryDur := time.Millisecond
+	for {
+		payment, err := t.provider.ExchangeTreasure(models.Treasure(treasure))
+		if err == nil {
+			for _, coin := range payment {
+				coins <- coin
+			}
+
+			return
+		}
+
+		<-time.After(retryDur)
+		retryDur *= 2
+	}
 }
 
 func (t *TreasuresExchanger) IncreaseWorkers(count int) {
-
 }
 
 func (t *TreasuresExchanger) ReduceWorkers(count int) {
